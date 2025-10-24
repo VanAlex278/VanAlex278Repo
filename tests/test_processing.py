640421c8074db31136c7d9c374f38971c6cacf77
Van Alex <van_alexandr@rambler.ru>
2025-10-24 20:17:02 +0300

    Fix test_processing.py and create test_widget

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,13 +1,8 @@
 from src.processing import sort_by_date, filter_by_state
 
 
-def test_sort_by_date():
-    assert sort_by_date([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]) == [
+def test_sort_by_date(test_list_of_operations):
+    assert sort_by_date(test_list_of_operations) == [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -15,13 +10,8 @@ def test_sort_by_date():
     ]
 
 
-def test_sort_by_date_false():
-    assert sort_by_date([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ], False) == [
+def test_sort_by_date_false(test_list_of_operations):
+    assert sort_by_date(test_list_of_operations, False) == [
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
@@ -29,13 +19,8 @@ def test_sort_by_date_false():
     ]
 
 
-def test_sort_by_date_true():
-    assert sort_by_date([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ], True) == [
+def test_sort_by_date_true(test_list_of_operations):
+    assert sort_by_date(test_list_of_operations, True) == [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
@@ -43,35 +28,20 @@ def test_sort_by_date_true():
     ]
 
 
-def test_filter_by_state():
-    assert filter_by_state([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ]) == [
+def test_filter_by_state(test_list_of_operations):
+    assert filter_by_state(test_list_of_operations) == [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
     ]
 
-def test_filter_by_state_executed():
-    assert filter_by_state([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ], 'EXECUTED') == [
+def test_filter_by_state_executed(test_list_of_operations):
+    assert filter_by_state(test_list_of_operations, 'EXECUTED') == [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
     ]
 
-def test_filter_by_state_canceled():
-    assert filter_by_state([
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
-    ], 'CANCELED') == [
+def test_filter_by_state_canceled(test_list_of_operations):
+    assert filter_by_state(test_list_of_operations, 'CANCELED') == [
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
---
+++ tests/test_widget.py
@@ -0,0 +1,5 @@
+#from src.widget import get_date
+
+
+#def test_get_date():
+#    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
