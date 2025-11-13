633429ea3daff3b969539401a90d45e2e6222d3f
Van Alex <van_alexandr@rambler.ru>
2025-11-13 22:55:24 +0400

    Fix utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,17 @@
 import json
-from idlelib.iomenu import encoding
+from json import JSONDecodeError
 
 
 def list_transaction_returned(path_to_json_file='../data/operations.json') -> list:
     """Функция чтения JSON-файла возвращает список словарей с данными о финансовых транзакциях"""
-    if path_to_json_file == None:
-        return []
-    else:
+    try:
         with open(path_to_json_file, encoding="utf-8") as f:
             list_transaction = json.load(f)
+            if list_transaction is None:
+                return []
+            elif type(list_transaction) is list:
+                return []
         return list_transaction
-
-data = list_transaction_returned()
-print(data)
+    except (FileNotFoundError, JSONDecodeError) as e:
+        print("Файл отсутствует или повреждён!")
+        return []
